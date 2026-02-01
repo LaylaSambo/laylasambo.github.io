31745559806e57f36c56e96cffcd6e974b2106ca
LaylaSambo <layla.sambo06@gmail.com>
2026-02-01 17:20:12 -0500

    setting up the pages

--- script.js
+++ script.js
@@ -2,6 +2,34 @@ const hoverSound = new Audio('./assets/hover.mp3');
 const clickSound = new Audio('./assets/click.mp3');
 const buttons = document.querySelectorAll('.shell-btn');
 
+
+const pages = {
+    btn1: `
+        <h1>About Me!</h1>
+        <p>yapper yapp yap about myself and intro to hobbies n stuff</p>
+    `,
+    btn2: `
+        <h1>Projects</h1>
+        <p>Work in progress....... </p>
+    `,
+    btn3: `
+        <h1>Exp</h1>
+        <p>My skills and experience here</p>
+    `,
+    btn4: `
+        <h1>Contact</h1>
+        <p>Feel free to reach out via email! :)</p>
+    `
+};
+
+function setPage(pageId) {
+    contentBox.innerHTML = pages[pageId];
+}
+
+setPage('btn1');
+
+
+
 buttons.forEach(button => {
     button.addEventListener('mouseenter', () => {
         hoverSound.currentTime = 0;
