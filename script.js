97a6f663c60ddabdd80a7d23528290d25af8c77d
LaylaSambo <layla.sambo06@gmail.com>
2026-02-01 18:00:02 -0500

    t

--- script.js
+++ script.js
@@ -1,35 +1,18 @@
 const hoverSound = new Audio('./assets/hover.mp3');
 const clickSound = new Audio('./assets/click.mp3');
 const buttons = document.querySelectorAll('.shell-btn');
-const contentBox = document.querySelector('.white-box-content');
+const pageText = document.getElementById('page-text');
 
-/*
 const pages = {
-    btn1: `
-        <h1>About Me!</h1>
-        <p>yapper yapp yap about myself and intro to hobbies n stuff</p>
-    `,
-    btn2: `
-        <h1>Projects</h1>
-        <p>Work in progress....... </p>
-    `,
-    btn3: `
-        <h1>Exp</h1>
-        <p>My skills and experience here</p>
-    `,
-    btn4: `
-        <h1>Contact</h1>
-        <p>Feel free to reach out via email! :)</p>
-    `
+    btn1: `<h1>About Me!</h1><p>yapper yapp yap about myself...</p>`,
+    btn2: `<h1>Projects</h1><p>Work in progress....... </p>`,
+    btn3: `<h1>Exp</h1><p>My skills and experience here</p>`,
+    btn4: `<h1>Contact</h1><p>Feel free to reach out!</p>`
 };
 
 function setPage(pageId) {
-    contentBox.innerHTML = pages[pageId];
+    pageText.innerHTML = pages[pageId];
 }
-*/
-
-
-
 
 buttons.forEach(button => {
     button.addEventListener('mouseenter', () => {
@@ -37,9 +20,9 @@ buttons.forEach(button => {
         hoverSound.play();
     });
 
-    button.addEventListener('click', () => {
+    button.addEventListener('click', (e) => {
         clickSound.currentTime = 0;
         clickSound.play();
-        setPage(e.target.id);
+        setPage(e.currentTarget.id);
     });
 });
